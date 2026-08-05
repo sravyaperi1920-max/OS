cda21754dbf06ceb853a8e8b97aaa6e93c8b45f2
sravyaperi1920-max <sravyaperi1920@gmail.com>
2026-08-05 09:48:16 +0530

    Delete 2520090059_Practical/2520090059_Practical-02 directory

--- 2520090059_Practical/2520090059_Practical-02/os11.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-int main()
-{
- printf("Before execlp()\n");
- execlp("date", "date", NULL);
- printf("This will not print if execlp() succeeds.\n");
- return 0;
-}
